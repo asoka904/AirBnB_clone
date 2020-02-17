215b45f24406fd0907bae18d0a792087cd50fb35
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-02-17 23:05:17 +0000

    Implemented create - console 0.1

--- console.py
+++ console.py
@@ -8,6 +8,10 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb) '
 
+    def preloop(self):
+        self.storage = FileStorage()
+        self.storage.reload()
+
     def do_quit(self, arg):
         'Quit command to exit the program'
         return True
@@ -19,6 +23,25 @@ class HBNBCommand(cmd.Cmd):
         'EOF command to exit the program'
         return True
 
+    def do_create(self, line):
+        'Creates a new instance of BaseModel and saves it '
+        if self.check_class(line):
+            base = BaseModel()
+            self.storage.new(base)
+            self.storage.save()
+            print(base.id)
+
+    def check_class(self, line):
+        commands = line.split()
+        for i, c in enumerate(commands):
+            if i is 0:
+                if c == 'BaseModel':
+                    return True
+                else:
+                    print("** class doesn't exist **")
+                    return False
+        print("** class name missing **")
+        return False
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
