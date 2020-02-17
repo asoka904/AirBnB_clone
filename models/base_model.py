a06ba0f682e0f5f1356f9be57ece33efcf3fc333
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-02-17 14:50:53 +0000

    changes various

--- models/base_model.py
+++ models/base_model.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python3
+""" This module define the BaseModel class """
 import uuid
 from datetime import datetime
-
-""" BaseModel class - Define all common attributes """
 
 
 class BaseModel:
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Create (or load) a new BaseModel"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
+        """Return a string with attributes of the instance"""
         return "[{}] ({}) {}"\
             .format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """Updates the attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ of the
+        instance"""
         my_dict = dict(self.__dict__)
         my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
         return my_dict
