946b14e461fe6dcd62d94dd41d7b05f283c54ad5
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-02-19 20:47:51 +0000

    Adding condition to new

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,8 +25,9 @@ class FileStorage:
 
     def new(self, obj):
         """sets in __objects the obj"""
-        key = str(obj.__class__.__name__) + "." + str(obj.id)
-        self.__objects[key] = obj
+        if obj:
+            key = str(obj.__class__.__name__) + "." + str(obj.id)
+            self.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file"""
