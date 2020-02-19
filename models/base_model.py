420f52eabd969c8fea551a5c1a4e590648982c26
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-02-19 01:11:31 +0000

    Removing models import

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" This module define the BaseModel class """
+"""
+This module define the BaseModel class
+"""
 
 import uuid
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
